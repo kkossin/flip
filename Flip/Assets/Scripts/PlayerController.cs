5ff658a1f52cd1765569a8f09ba024a31c39cc65
kkossin <kmkossin@gmail.com>
2016-10-02 22:58:19 -0600

    Jumping and flipping functional

--- Flip/Assets/Scripts/PlayerController.cs
+++ Flip/Assets/Scripts/PlayerController.cs
@@ -2,14 +2,80 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+    public float jumpSpeed;
+    public float fallSpeed;
+    public float gravity;
+    public bool grounded;
+    public bool flipped;
 
-	// Use this for initialization
-	void Start () {
+    private CharacterController controller; 
+
+    void Start () {
+        controller = GetComponent<CharacterController>();
+        grounded = true;
+        flipped = false;
+    }
 	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
-	}
+        isGrounded();
+        jump();
+        fall();
+        flip();
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            fallSpeed = -jumpSpeed;
+        }
+    }
+
+    void fall()
+    {
+        if (!flipped)
+        {
+            if (!grounded)
+            {
+                fallSpeed += gravity * Time.deltaTime;
+            }
+            else
+            {
+                if (fallSpeed > 0) fallSpeed = 0;
+            }
+            controller.Move(new Vector3(0, -fallSpeed) * Time.deltaTime);
+        }
+        else
+        {
+            if (!grounded)
+            {
+                fallSpeed += -gravity * Time.deltaTime;
+            }
+            else
+            {
+                if (fallSpeed < 0) fallSpeed = 0;
+            }
+            controller.Move(new Vector3(0, -fallSpeed) * Time.deltaTime);
+        }
+    }
+
+    void flip()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            flipped = !flipped;
+        }
+    }
+
+    void isGrounded()
+    {
+        if (!flipped)
+        {
+            grounded = (Physics.Raycast(transform.position, -transform.up, controller.height));
+        }
+        else
+        {
+            grounded = (Physics.Raycast(transform.position, transform.up, controller.height));
+        }
+    }
 }
