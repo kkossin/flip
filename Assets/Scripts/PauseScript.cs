60bb7d1ae7ef624ad87123990765999ce270cf9e
kkossin <kmkossin@gmail.com>
2016-11-07 18:09:38 -0700

    Pause Script added

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour
+{
+
+    bool Pause = false;
+
+    void Update()
+    {
+
+        if (Pause == false)
+        {
+            Time.timeScale = 1;
+        }
+
+        else
+        {
+            Time.timeScale = 0;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Pause == true)
+            {
+                Pause = false;
+            }
+
+            else
+            {
+                Pause = true;
+            }
+        }
+
+
+    }
+
+
+}
