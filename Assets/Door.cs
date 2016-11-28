2cfc4d4d88f6db7ed2ba7fef509ce2f724b5b245
kkossin <kmkossin@gmail.com>
2016-11-28 00:33:06 -0700

    moving doors added

---
+++ Assets/Door.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+    private float speedVerti;
+
+	void Start () {
+        speedVerti = 0.01f;
+	}
+	
+	void FixedUpdate () {
+        if (this.transform.position.y > -3.0f)
+        {
+            this.transform.position = new Vector2(this.transform.position.x, this.transform.position.y + speedVerti);
+        }
+
+        if (speedVerti < 0.0f && this.transform.position.y < -1.0f)
+        {
+            speedVerti = -speedVerti;
+        }
+        if (speedVerti > 0.0f && this.transform.position.y > 1.0f)
+        {
+            speedVerti = -speedVerti;
+        }
+    }
+}
